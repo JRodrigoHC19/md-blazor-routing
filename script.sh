61f6b22c8c304307fb30660403a7b47d0d5899ee
JRodrigoHC19 <jaime.huacac@tecsup.edu.pe>
2024-09-30 21:56:41 -0500

    v2

--- script.sh
+++ script.sh
@@ -7,7 +7,16 @@ ID_IMAGE=img-pizza
 git clone https://github.com/MicrosoftDocs/mslearn-blazor-navigation.git BlazingPizza
 
 # Preparation
-cp ./Dockerfile ./BlazingPizza/Dockerfile && cd BlazingPizza
+rm ./BlazingPizza/OrderState.cs
+rm ./BlazingPizza/PizzaStoreContext.cs
+rm ./BlazingPizza/App.razor
+rm ./BlazingPizza/Pages/_Host.cshtml
+rm ./BlazingPizza/Pages/Index.razor
+rm ./BlazingPizza/Pages/Checkout.razor
+rm ./BlazingPizza/Pages/OrderDetail.razor
+rm ./BlazingPizza/Pages/MyOrders.razor
+cp files/* ./BlazingPizza/
+cd BlazingPizza
 
 # Execution
 docker build -t $ID_IMAGE .
