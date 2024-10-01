ed04e358269e526c14fda98934fe43f3e4662c68
JRodrigoHC19 <jaime.huacac@tecsup.edu.pe>
2024-09-30 19:49:03 -0500

    beta

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Variables
+ID_IMAGE=img-pizza
+
+# Repository
+git clone https://github.com/MicrosoftDocs/mslearn-blazor-navigation.git BlazingPizza
+
+# Preparation
+cp ./Dockerfile ./BlazingPizza/Dockerfile && cd BlazingPizza
+
+# Execution
+dotnet build -t $ID_IMAGE .
+docker run -it --rm -p 8080:80 $ID_IMAGE
+docker rmi $ID_IMAGE
