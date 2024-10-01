5601d7ce7afb5ee6525b959b9ef4c1a07b383557
JRodrigoHC19 <jaime.huacac@tecsup.edu.pe>
2024-09-30 22:03:25 -0500

    v2.1

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,8 @@ FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
 WORKDIR /src
 COPY ./BlazingPizza.csproj . 
 RUN dotnet restore
-COPY . .
+COPY ./BlazingPizza .
+COPY ./files ./BlazingPizza
 RUN dotnet publish -c release -o /app
 
 
--- script.sh
+++ script.sh
@@ -7,16 +7,7 @@ ID_IMAGE=img-pizza
 git clone https://github.com/MicrosoftDocs/mslearn-blazor-navigation.git BlazingPizza
 
 # Preparation
-rm ./BlazingPizza/OrderState.cs
-rm ./BlazingPizza/PizzaStoreContext.cs
-rm ./BlazingPizza/App.razor
-rm ./BlazingPizza/Pages/_Host.cshtml
-rm ./BlazingPizza/Pages/Index.razor
-rm ./BlazingPizza/Pages/Checkout.razor
-rm ./BlazingPizza/Pages/OrderDetail.razor
-rm ./BlazingPizza/Pages/MyOrders.razor
-cp files/* ./BlazingPizza/
-cd BlazingPizza
+cp ./Dockerfile ./BlazingPizza/Dockerfile && cd ./BlazingPizza
 
 # Execution
 docker build -t $ID_IMAGE .
